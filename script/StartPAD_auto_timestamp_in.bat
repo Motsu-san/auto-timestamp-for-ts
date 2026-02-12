9b3d63481e1652ebbd214a74315f684476c4d2a1
Motsu-san <83898149+Motsu-san@users.noreply.github.com>
2026-02-12 10:15:03 +0900

    chore: add comments
    Signed-off-by: Motsu-san <83898149+Motsu-san@users.noreply.github.com>

--- script/StartPAD_auto_timestamp_in.bat
+++ script/StartPAD_auto_timestamp_in.bat
@@ -1,5 +1,7 @@
 @if not "%~0"=="%~dp0.\%~nx0" start /min cmd /c,"%~dp0.\%~nx0" %* & goto :eof
 @echo off
+
+@REM Restrict changes to environment variables to the local scope.
 setlocal
 
 cd /d %~dp0
